8f7b83e7952c5c7360451f0a7a302a853ab27312
JENWS <59334188+tonysum33@users.noreply.github.com>
2025-01-25 12:13:19 +0000

    OK

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,25 +20,27 @@ intputData['ForceP'] = st.sidebar.number_input('樁頂垂直力(tf)')
 intputData['ForceH'] = st.sidebar.number_input('樁頂水平力(tf)')
 
 
-st.title("基樁側向")
+
+st.markdown('## 基樁側向分析')
 st.divider()
 
-st.markdown('## 輸入資料')
-st.markdown('### 基樁資料')
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown('### 基樁資料')
+    st.write("樁徑 D =",intputData["PileD"],"cm",)
+    st.write("樁長 L =",intputData["PileL"],"m",)
+    st.write("樁頂距地面高 Ht =",intputData["Ht"],"cm",)
+    st.write("混凝土強度 fc' =",intputData["Fc"],"kgf/cm2",)
+    st.write("水平地盤反力係數",intputData['kh'],"kgf/cm3")
 
-st.write("樁徑 D =",intputData["PileD"],"cm",)
-st.write("樁長 L =",intputData["PileL"],"m",)
-st.write("樁頂距地面高 Ht =",intputData["Ht"],"cm",)
-st.write("混凝土強度 fc' =",intputData["Fc"],"kgf/cm2",)
-st.write("水平地盤反力係數",intputData['kh'],"kgf/cm3")
-
-st.markdown('### 外力資料')
-st.write("樁頂垂直力 P =",intputData['ForceP'],"tf",)
-st.write("樁頂水平力 P =",intputData['ForceH'],"tf",)
+with col2:
+    st.markdown('### 外力資料')
+    st.write("樁頂垂直力 P =",intputData['ForceP'],"tf",)
+    st.write("樁頂水平力 P =",intputData['ForceH'],"tf",)
 
 st.divider()
 Ap = 1/4 * math.pi * intputData['PileD']**2
-Ip = 1/64* math.pi * intputData['PileD']**4
+Ip = 1/64 * math.pi * intputData['PileD']**4
 Ep = 15100 * intputData['Fc']**0.5
 beta = ((intputData['kh']*intputData['PileD'])/(4*Ep*Ip))**0.25
 

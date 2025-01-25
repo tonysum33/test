87aebc0f798afb1994ac6e2c3eeb92a218aeda57
JENWS <59334188+tonysum33@users.noreply.github.com>
2025-01-25 11:46:10 +0000

    OK

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,50 @@
 import streamlit as st
 import math
+import pandas as pd
 
 
-option = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
-)
+intputData = {
+    'PileD': None,
+    'PileL': None,
+    'Fc':    None,
+    'Ht':    None,
+    'kh' :   None
+    }
+
+intputData['PileD'] = st.sidebar.number_input('樁徑(cm)',value=100, min_value =0)
+intputData['PileL'] = st.sidebar.number_input('樁長(m)',value=20,min_value =0)
+intputData['Ht'] = st.sidebar.number_input('樁頂距地面高(cm)',value=0, min_value =0)
+intputData['Fc'] = st.sidebar.number_input('混凝土強度(kgf/cm2)',value=245)
+intputData['kh'] = st.sidebar.number_input('水平地盤反力係數(kgf/cm3)',format="%0.3f")
+intputData['ForceP'] = st.sidebar.number_input('樁頂垂直力(tf)')
+intputData['ForceH'] = st.sidebar.number_input('樁頂水平力(tf)')
 
 
-pileData = {'D':  150,
-            'L': 4000,
-            'Fc': 245,
-            'Ht':  10,
-            }
-kh = 0.992
+st.title("基樁側向")
+st.divider()
 
-Ap = 1/4 * math.pi * pileData["D"]**2
-Ip = 1/64* math.pi * pileData["D"]**4
-Ep = 15100 * pileData["Fc"]**0.5
-beta = ((kh*pileData["D"])/(4*Ep*Ip))**0.25
+st.markdown('## 輸入資料')
+st.markdown('### 基樁資料')
 
+st.write("樁徑 D =",intputData["PileD"],"cm",)
+st.write("樁長 L =",intputData["PileL"],"m",)
+st.write("樁頂距地面高 Ht =",intputData["Ht"],"cm",)
+st.write("混凝土強度 fc' =",intputData["Fc"],"kgf/cm2",)
+st.write("水平地盤反力係數",intputData['kh'],"kgf/cm3")
 
-st.markdown('# 計算結果')
-st.write('{:10} {:12.1f} {:10}{:5}'.format("D =",pileData["D"]/100,"m","樁徑")) 
-st.write('{:10} {:12.1f} {:10}{:5}'.format("L =",pileData["L"]/100,"m","樁長")) 
-st.write('{:10} {:12.1f} {:10}{:5}'.format("Ht =",pileData["Ht"],"cm","樁頂距地面高"))
-st.write('{:10} {:12.1f} {:10}{:5}'.format("fc' =",pileData["Fc"],"cm2","混凝土強度")) 
-st.write('{:10} {:12.1f} {:10}{:5}'.format("Ap =",Ap,"cm2","基樁面積")) 
-st.write('{:10} {:12.1f} {:10}{:5}'.format("Ip =",Ip,"cm4","慣性矩"))
-st.write('{:10} {:12.1f} {:10}{:5}'.format("Ep =",Ep,"kgf/cm2","彈性係數"))
-st.write('{:10} {:12.3f} {:10}{:5}'.format("kh =",kh,"kgf/cm3","水平地盤反力係數"))
+st.markdown('### 外力資料')
+st.write("樁頂垂直力 P =",intputData['ForceP'],"tf",)
+st.write("樁頂水平力 P =",intputData['ForceH'],"tf",)
 
+st.divider()
+Ap = 1/4 * math.pi * intputData['PileD']**2
+Ip = 1/64* math.pi * intputData['PileD']**4
+Ep = 15100 * intputData['Fc']**0.5
+beta = ((intputData['kh']*intputData['PileD'])/(4*Ep*Ip))**0.25
 
-st.write("You selected:", option)
+st.markdown('## 輸出資料')
+st.write("斷面積 =" ,Ap ,"cm2")
+st.write("慣性矩 =" ,Ip ,"cm4")
+st.write("彈性係數 =" ,Ep ,"kgf/cm2")
+st.write("β =" ,beta*100 ,"1/m")
+st.write("βL =" ,beta*intputData["PileL"]*100 ,"m")

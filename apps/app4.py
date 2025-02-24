0c946de8e1e50b17ae71935b566915270b145626
JENWS <59334188+tonysum33@users.noreply.github.com>
2025-02-24 14:08:07 +0000

    OK

---
+++ apps/app4.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("工具應用程式4")
+
+
+import math
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,9 @@ import streamlit as st
 a1 = st.Page("apps/app1.py", title="1.耐震設計")
 a2 = st.Page("apps/app2.py", title="2.基礎設計")
 a3 = st.Page("apps/app3.py", title="3.基樁側向分析")
+a4 = st.Page("apps/app4.py", title="4.混凝土分析")
 
  
-pg = st.navigation({"工具程式": [a1, a2, a3]})
+pg = st.navigation({"工具程式": [a1, a2, a3, a4]})
 
 pg.run()

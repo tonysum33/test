158bb31eca42a109307c218c7c6160b83a5966ae
JENWS <59334188+tonysum33@users.noreply.github.com>
2025-02-25 00:56:08 +0000

    OK

--- apps/app4.py
+++ apps/app4.py
@@ -1,5 +1,37 @@
 import streamlit as st
 st.title("工具應用程式4")
 
+import numpy as np
+from concreteproperties.material import Concrete, SteelBar
+from concreteproperties.stress_strain_profile import (
+    ConcreteLinear,
+    RectangularStressBlock,
+    SteelElasticPlastic,
+)
+from sectionproperties.pre.library.concrete_sections import concrete_rectangular_section
 
-import math
+concrete = Concrete(
+    name="32 MPa Concrete",
+    density=2.4e-6,
+    stress_strain_profile=ConcreteLinear(elastic_modulus=30.1e3),
+    ultimate_stress_strain_profile=RectangularStressBlock(
+        compressive_strength=32,
+        alpha=0.802,
+        gamma=0.89,
+        ultimate_strain=0.003,
+    ),
+    flexural_tensile_strength=3.4,
+    colour="lightgrey",
+)
+
+steel = SteelBar(
+    name="500 MPa Steel",
+    density=7.85e-6,
+    stress_strain_profile=SteelElasticPlastic(
+        yield_strength=500,
+        elastic_modulus=200e3,
+        fracture_strain=0.05,
+    ),
+    colour="grey",
+)
+

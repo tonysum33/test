7d0d9e2f901203770e207ecbc48cd45d29b53cf4
JENWS <59334188+tonysum33@users.noreply.github.com>
2025-03-22 03:48:14 +0000

    OK

--- apps/app5.py
+++ apps/app5.py
@@ -31,3 +31,65 @@ ops.fix(3, 1, 1, 0)
 
 opsv.plot_model()
 plt.title('plot_model before defining elements')
+
+ops.geomTransf('Linear', 1)
+
+# columns
+ops.element('elasticBeamColumn', 1, 1, 2, Acol, E, IzCol, 1)
+ops.element('elasticBeamColumn', 2, 3, 4, Acol, E, IzCol, 1)
+# girder
+ops.element('elasticBeamColumn', 3, 2, 4, Agir, E, IzGir, 1)
+
+Px = 2.e+3
+Wy = -10.e+3
+Wx = 0.
+
+Ew = {3: ['-beamUniform', Wy, Wx]}
+
+ops.timeSeries('Constant', 1)
+ops.pattern('Plain', 1, 1)
+ops.load(2, Px, 0., 0.)
+
+for etag in Ew:
+    ops.eleLoad('-ele', etag, '-type', Ew[etag][0], Ew[etag][1],
+                Ew[etag][2])
+
+ops.constraints('Transformation')
+ops.numberer('RCM')
+ops.system('BandGeneral')
+ops.test('NormDispIncr', 1.0e-6, 6, 2)
+ops.algorithm('Linear')
+ops.integrator('LoadControl', 1)
+ops.analysis('Static')
+ops.analyze(1)
+
+ops.printModel()
+
+opsv.plot_model()
+plt.title('plot_model after defining elements')
+
+opsv.plot_loads_2d()
+
+# sfac = 80.
+
+opsv.plot_defo()
+# opsv.plot_defo(sfac)
+# fmt_interp = {'color': 'blue', 'linestyle': 'solid', 'linewidth': 1.2, 'marker': '.', 'markersize': 6}
+# opsv.plot_defo(sfac, fmt_interp=fmt_interp)
+
+# 4. plot N, V, M forces diagrams
+
+sfacN, sfacV, sfacM = 5.e-5, 5.e-5, 5.e-5
+
+opsv.section_force_diagram_2d('N', sfacN)
+plt.title('Axial force distribution')
+
+opsv.section_force_diagram_2d('T', sfacV)
+plt.title('Shear force distribution')
+
+opsv.section_force_diagram_2d('M', sfacM)
+plt.title('Bending moment distribution')
+
+plt.show()
+
+exit()

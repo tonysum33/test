0dcfa54765fe26f666245a9900bdc7cd0797d6d0
JENWS <59334188+tonysum33@users.noreply.github.com>
2025-03-22 02:14:11 +0000

    OK

---
+++ apps/app5.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.title("工具應用程式5")
+
+import openseespy.opensees as ops
+import opsvis as opsv
+
+import matplotlib.pyplot as plt
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ a1 = st.Page("apps/app1.py", title="1.耐震設計")
 a2 = st.Page("apps/app2.py", title="2.基礎設計")
 a3 = st.Page("apps/app3.py", title="3.基樁側向分析")
 a4 = st.Page("apps/app4.py", title="4.混凝土分析")
-
+a5 = st.Page("apps/app5.py", title="5.OpenSee")
  
-pg = st.navigation({"工具程式": [a1, a2, a3, a4]})
+pg = st.navigation({"工具程式": [a1, a2, a3, a4, a5]})
 
 pg.run()
